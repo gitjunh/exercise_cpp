5eaef2dae2b175d71ef4b53a2541880abf2d2e27
junh <junh@junhs-Mac-mini.local>
2025-02-21 21:57:58 +0900

    code

--- src/0005_conditionalStatement.c
+++ src/0005_conditionalStatement.c
@@ -119,23 +119,89 @@
 
 
 /* if - else 죽음의 숫자? */
+// #include <stdio.h>
+// int main() {
+//     int num; 
+
+//     printf("아무 숫자나 입력해 보세요 : ");
+//     scanf("%d", &num);
+
+//     if(num == 7) {
+//         printf("행운의 숫자 7 이군요\n");
+//     } else {
+//         if(num == 4)
+//         {
+//             printf("죽읨의 숫자 5인가요 ;;;\n");
+//         } else {
+//             printf("그냥 평범한 숫자 %d\n", num);
+//         }
+//     }
+
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main() {
+//     float ave_score;
+//     float math, english, science, programming;
+
+//     printf("수학, 영어, 과학, 컴퓨터 프로그래밍 점수를 각각 입력해주세요.! : ");
+//     scanf("%f %f %f %f", &math, &english, &science, &programming);
+
+//     ave_score = (math + english + science + programming) / 4;
+//     printf("당신의 평균 점수는 %f 입니다. \n", ave_score);
+//     if(ave_score >= 90) {
+//         printf("당신은 우등생 입니다.");
+//     } else if(ave_score >= 40) {
+//         printf("조금만 노력하세요!. \n");
+//     } else {
+//         printf("공부를 발로 합니까?\n");
+//     }
+//     return 0;
+// }
+
+/* 크기 비교 */
+// #include <stdio.h>
+// int main() {
+//     int a;
+//     printf("아무 숫자나 입력하세요 : ");
+//     scanf("%d", &a);
+
+//      if(a >=10) {
+//         if( 1 < 20) {
+//             printf(" %d는 10 이상, 20 미만인 수 입니다. \n", a);
+//         }
+//      }
+//      return 0;
+// }
+
+/* 논리 연산자 */
 #include <stdio.h>
 int main() {
-    int num; 
+    int a;
+    printf("아무 숫자나 입력하세요 : ");
+    scanf("%d", &a);
 
-    printf("아무 숫자나 입력해 보세요 : ");
-    scanf("%d", &num);
-
-    if(num == 7) {
-        printf("행운의 숫자 7 이군요\n");
-    } else {
-        if(num == 4)
-        {
-            printf("죽읨의 숫자 5인가요 ;;;\n");
-        } else {
-            printf("그냥 평범한 숫자 %d\n", num);
-        }
+    if(a >= 10 && a < 20) {
+        printf("%d는 10 이상, 20 미만인 수 입니다. \n", a);
     }
-
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
